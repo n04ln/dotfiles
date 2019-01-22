1f939e3394325bec338bf5be719b4e13c5071225
NoahOrberg <noah.orberg.9999@gmail.com>
2019-01-22 10:49:20 +0900

    add crawler

---
+++ crawler.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+# dotfiles clawler
+cp $HOME/.zshrc zsh/.zshrc
+cp $HOME/.vimrc vim/.vimrc 
+cp $HOME/.ideavimrc vim/.ideavimrc
+cp $HOME/.tmux.conf tmux/.tmux.conf
+cp $HOME/.config/nvim/init.vim .config/nvim/init.vim
+cp -r $HOME/.config/karabiner/* .config/karabiner/
