c69375acfc435aa8d7cceb173c612195e688acff
NoahOrberg <noah.orberg.9999@gmail.com>
2019-04-01 01:10:33 +0900

    impl diff option

--- setup.zsh
+++ setup.zsh
@@ -1,8 +1,17 @@
 #!/bin/zsh
 
 local -A opts
-zparseopts -D -A opts -help h -force f
+zparseopts -D -A opts -help h -force f -diff d
 
+# file location in dotfiles repository
+zshrc_path=zsh/.zshrc
+vimrc_path=vim/.vimrc
+ideavimrc_path=vim/.ideavimrc
+tmux_conf_path=tmux/.tmux.conf
+nvim_config_path=.config/nvim
+karabiner_config_path=.config/karabiner
+
+# sub-functions for replace dotfiles
 _mv_config_file() {
     # NOTE:
     #   $1: the config file name
@@ -56,6 +65,7 @@ _mv_config_file() {
     fi
 }
 
+#_show_help: show help message
 _show_help() {
     echo "setup.zsh is the Installer of NoahOrberg's dotfiles"
     echo "It setup .vimrc, .ideavimrc, .tmux.conf, init.vim(for neovim) and karabiner config file"
@@ -63,6 +73,21 @@ _show_help() {
     echo "Options:"
     echo "  -f, --force ... force replacement"
     echo "  -h, --help  ... show this message"
+    echo "  -d, --diff  ... save the difference(patch file) of each dotfiles to diff/"
+    exit 0
+}
+
+# _diff: use diff command and redirect this output to diff/*
+_diff() {
+    if [ ! -e ./diff ] && mkdir -p diff/
+    echo "make diff files to diff/*..."
+    diff -up ${HOME}/.zshrc ${zshrc_path} > diff/zshrc.patch
+    diff -up ${HOME}/.vimrc ${vimrc_path} > diff/.vimrc.patch
+    diff -up ${HOME}/.ideavimrc ${ideavimrc_path} > diff/ideavimrc.patch
+    diff -up ${HOME}/.tmux.conf ${tmux_conf_path} > diff/tmux.conf.patch
+    diff -up ${HOME}/.config/nvim ${nvim_config_path} > diff/nvim.patch
+    diff -up ${HOME}/.config/karabiner ${karabiner_config_path} > diff/karabiner.patch
+    echo "done!"
     exit 0
 }
 
@@ -70,16 +95,14 @@ _show_help() {
 [[ -n "${opts[(i)--help]}" ]] && _show_help
 [[ -n "${opts[(i)-h]}" ]] && _show_help
 
-# MOVE CONFIG FILES
-# .zshrc
-_mv_config_file .zshrc zsh/.zshrc $HOME/.zshrc
-# .vimrc
-_mv_config_file .vimrc vim/.vimrc $HOME/.vimrc
-# .ideavimrc
-_mv_config_file .ideavimrc vim/.ideavimrc $HOME/.ideavimrc
-# .tmux.conf_bk
-_mv_config_file .tmux_conf tmux/.tmux.conf $HOME/.tmux.conf
-# .config/nvim
-_mv_config_file .config/nvim .config/nvim $HOME/.config/nvim
-# .config/karabiner
-_mv_config_file .config/karabiner .config/karabiner $HOME/.config/karabiner
+# save diff files to diff/
+[[ -n "${opts[(i)--diff]}" ]] && _diff
+[[ -n "${opts[(i)-d]}" ]] && _diff
+
+# if option is not specified or select `-f` option, move config files
+_mv_config_file .zshrc ${zshrc_path} ${HOME}/.zshrc
+_mv_config_file .vimrc ${vimrc_path} ${HOME}/.vimrc
+_mv_config_file .ideavimrc ${ideavimrc_path} ${HOME}/.ideavimrc
+_mv_config_file .tmux_conf ${tmux_conf_path} ${HOME}/.tmux.conf
+_mv_config_file .config/nvim ${nvim_config_path} ${HOME}/.config/nvim
+_mv_config_file .config/karabiner ${karabiner_config_path} ${HOME}/.config/karabiner
